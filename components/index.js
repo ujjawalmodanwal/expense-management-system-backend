529bf7f4d45d8b99ab5c41edc8e5d57882b9e2bb
Ujjawal Modanwal <51051403+ujjawalmodanwal@users.noreply.github.com>
2022-12-25 23:55:44 +0530

    added cors

--- components/index.js
+++ components/index.js
@@ -7,6 +7,7 @@ const { errorHandler, notFound } = require('../middlewares/errorMiddlewares.js')
 const cardRoutes = require("../Routes/cardRoutes");
 const tableRoutes = require("../Routes/tableRoutes");
 const path= require("path")
+const cors = require('cors')
 
 const app = express();
 dotenv.config();
@@ -15,6 +16,17 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 
+var cors = require(cors());
+app.use(cors());
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();  
+}
+app.use(allowCrossDomain);
 
 app.use("/api/cards", cardRoutes);
 app.use("/api/users", userRoutes);
